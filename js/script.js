0d737c2bb492a1287bc02320d57eeda03af93729
seymur <seymur-98.98@mail.ru>
2017-12-20 19:45:38 +0400

    icon effect

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$("#inlineFormInputGroup").click(function(){
+    $(".input-group-addon").addClass("color-org");
+
+});
+
+
+
+
+// $(".icon2").hover(function(){
+//     $(this).addClass("color-org");
+//     $(this).css("transform","scale(2,3)")
+//     }, function(){
+//     $(this).removeClass("color-org");
+//      $(this).css("transform","scale(1,1)")
+
+// });
+
+
+
+
+// $(document).ready(function(){
+//     $("p").hover(function(){
+//         $(this).css("background-color", "yellow");
+//         }, function(){
+//         $(this).css("background-color", "pink");
+//     });
+// });
